045abf9e4d4526a2a6e1283035abd9c8732d39c5
aayushk18 <kumaraayush1807@gmail.com>
2026-02-28 17:50:35 +0530

    commit

--- src/app/(public)/contact/page.tsx
+++ src/app/(public)/contact/page.tsx
@@ -256,116 +256,31 @@ export default function Contact() {
                     Phone Number
                   </label>
 
-                  <div className="flex gap-2">
-                    {/* Country Code */}
+                  <div className="flex flex-wrap gap-2">
                     <select
                       value={countryCode}
                       onChange={(e) => setCountryCode(e.target.value)}
-                      className="w-32 px-3 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent bg-white"
+                      className="w-28 px-3 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent bg-white"
                     >
                       <option value="+1">🇺 +1 (USA/Canada)</option>
                       <option value="+7">🇷 +7 (Russia)</option>
                       <option value="+20"> +20 (Egypt)</option>
                       <option value="+27">🇿 +27 (South Africa)</option>
-                      <option value="+30"> +30 (Greece)</option>
+
                       <option value="+31"> +31 (Netherlands)</option>
-                      <option value="+32"> +32 (Belgium)</option>
-                      <option value="+33"> +33 (France)</option>
-                      <option value="+34"> +34 (Spain)</option>
-                      <option value="+36"> +36 (Hungary)</option>
-                      <option value="+39"> +39 (Italy)</option>
-                      <option value="+40"> +40 (Romania)</option>
-                      <option value="+41"> +41 (Switzerland)</option>
-                      <option value="+43"> +43 (Austria)</option>
-                      <option value="+44"> +44 (UK)</option>
-                      <option value="+45"> +45 (Denmark)</option>
-                      <option value="+46"> +46 (Sweden)</option>
-                      <option value="+47"> +47 (Norway)</option>
-                      <option value="+48"> +48 (Poland)</option>
-                      <option value="+49"> +49 (Germany)</option>
-                      <option value="+51"> +51 (Peru)</option>
-                      <option value="+52"> +52 (Mexico)</option>
-                      <option value="+53"> +53 (Cuba)</option>
-                      <option value="+54"> +54 (Argentina)</option>
-                      <option value="+55"> +55 (Brazil)</option>
-                      <option value="+56"> +56 (Chile)</option>
-                      <option value="+57"> +57 (Colombia)</option>
-                      <option value="+58"> +58 (Venezuela)</option>
-                      <option value="+60"> +60 (Malaysia)</option>
-                      <option value="+61"> +61 (Australia)</option>
-                      <option value="+62"> +62 (Indonesia)</option>
-                      <option value="+63"> +63 (Philippines)</option>
-                      <option value="+64"> +64 (New Zealand)</option>
-                      <option value="+65"> +65 (Singapore)</option>
-                      <option value="+66"> +66 (Thailand)</option>
-                      <option value="+81"> +81 (Japan)</option>
-                      <option value="+82"> +82 (South Korea)</option>
-                      <option value="+84"> +84 (Vietnam)</option>
-                      <option value="+86"> +86 (China)</option>
-                      <option value="+90"> +90 (Turkey)</option>
-                      <option value="+91"> +91 (India)</option>
-                      <option value="+92"> +92 (Pakistan)</option>
-                      <option value="+93"> +93 (Afghanistan)</option>
-                      <option value="+94"> +94 (Sri Lanka)</option>
-                      <option value="+95"> +95 (Myanmar)</option>
-                      <option value="+98"> +98 (Iran)</option>
-                      <option value="+211"> +211 (South Sudan)</option>
-                      <option value="+212"> +212 (Morocco)</option>
-                      <option value="+213"> +213 (Algeria)</option>
-                      <option value="+216"> +216 (Tunisia)</option>
-                      <option value="+218"> +218 (Libya)</option>
-                      <option value="+220"> +220 (Gambia)</option>
-                      <option value="+221"> +221 (Senegal)</option>
-                      <option value="+234"> +234 (Nigeria)</option>
-                      <option value="+254"> +254 (Kenya)</option>
-                      <option value="+255"> +255 (Tanzania)</option>
-                      <option value="+256"> +256 (Uganda)</option>
-                      <option value="+263"> +263 (Zimbabwe)</option>
-                      <option value="+351"> +351 (Portugal)</option>
-                      <option value="+352"> +352 (Luxembourg)</option>
-                      <option value="+353"> +353 (Ireland)</option>
-                      <option value="+354"> +354 (Iceland)</option>
-                      <option value="+355"> +355 (Albania)</option>
-                      <option value="+356"> +356 (Malta)</option>
-                      <option value="+357"> +357 (Cyprus)</option>
-                      <option value="+358"> +358 (Finland)</option>
-                      <option value="+359"> +359 (Bulgaria)</option>
-                      <option value="+370"> +370 (Lithuania)</option>
-                      <option value="+371"> +371 (Latvia)</option>
-                      <option value="+372"> +372 (Estonia)</option>
-                      <option value="+380"> +380 (Ukraine)</option>
-                      <option value="+381"> +381 (Serbia)</option>
-                      <option value="+385"> +385 (Croatia)</option>
-                      <option value="+386"> +386 (Slovenia)</option>
-                      <option value="+420"> +420 (Czech Republic)</option>
-                      <option value="+421"> +421 (Slovakia)</option>
-                      <option value="+880"> +880 (Bangladesh)</option>
-                      <option value="+971"> +971 (UAE)</option>
-                      <option value="+972"> +972 (Israel)</option>
-                      <option value="+973"> +973 (Bahrain)</option>
-                      <option value="+974"> +974 (Qatar)</option>
-                      <option value="+975"> +975 (Bhutan)</option>
-                      <option value="+976"> +976 (Mongolia)</option>
-                      <option value="+977"> +977 (Nepal)</option>
-                      <option value="+992"> +992 (Tajikistan)</option>
-                      <option value="+993"> +993 (Turkmenistan)</option>
-                      <option value="+994"> +994 (Azerbaijan)</option>
-                      <option value="+995"> +995 (Georgia)</option>
-                      <option value="+996"> +996 (Kyrgyzstan)</option>
-                      <option value="+998"> +998 (Uzbekistan)</option>
+                      <option value="+32"> +32 (Belgium)</option> <option value="+33"> +33 (France)</option> <option value="+34"> +34 (Spain)</option> <option value="+36"> +36 (Hungary)</option> <option value="+39"> +39 (Italy)</option> <option value="+40"> +40 (Romania)</option> <option value="+41"> +41 (Switzerland)</option> <option value="+43"> +43 (Austria)</option> <option value="+44"> +44 (UK)</option> <option value="+45"> +45 (Denmark)</option> <option value="+46"> +46 (Sweden)</option> <option value="+47"> +47 (Norway)</option> <option value="+48"> +48 (Poland)</option> <option value="+49"> +49 (Germany)</option> <option value="+51"> +51 (Peru)</option> <option value="+52"> +52 (Mexico)</option> <option value="+53"> +53 (Cuba)</option> <option value="+54"> +54 (Argentina)</option> <option value="+55"> +55 (Brazil)</option> <option value="+56"> +56 (Chile)</option> <option value="+57"> +57 (Colombia)</option> <option value="+58"> +58 (Venezuela)</option> <option value="+60"> +60 (Malaysia)</option> <option value="+61"> +61 (Australia)</option> <option value="+62"> +62 (Indonesia)</option> <option value="+63"> +63 (Philippines)</option> <option value="+64"> +64 (New Zealand)</option> <option value="+65"> +65 (Singapore)</option> <option value="+66"> +66 (Thailand)</option> <option value="+81"> +81 (Japan)</option> <option value="+82"> +82 (South Korea)</option> <option value="+84"> +84 (Vietnam)</option> <option value="+86"> +86 (China)</option> <option value="+90"> +90 (Turkey)</option> <option value="+91"> +91 (India)</option> <option value="+92"> +92 (Pakistan)</option> <option value="+93"> +93 (Afghanistan)</option> <option value="+94"> +94 (Sri Lanka)</option> <option value="+95"> +95 (Myanmar)</option> <option value="+98"> +98 (Iran)</option> <option value="+211"> +211 (South Sudan)</option> <option value="+212"> +212 (Morocco)</option> <option value="+213"> +213 (Algeria)</option> <option value="+216"> +216 (Tunisia)</option> <option value="+218"> +218 (Libya)</option> <option value="+220"> +220 (Gambia)</option> <option value="+221"> +221 (Senegal)</option> <option value="+234"> +234 (Nigeria)</option> <option value="+254"> +254 (Kenya)</option> <option value="+255"> +255 (Tanzania)</option> <option value="+256"> +256 (Uganda)</option> <option value="+263"> +263 (Zimbabwe)</option> <option value="+351"> +351 (Portugal)</option> <option value="+352"> +352 (Luxembourg)</option> <option value="+353"> +353 (Ireland)</option> <option value="+354"> +354 (Iceland)</option> <option value="+355"> +355 (Albania)</option> <option value="+356"> +356 (Malta)</option> <option value="+357"> +357 (Cyprus)</option> <option value="+358"> +358 (Finland)</option> <option value="+359"> +359 (Bulgaria)</option> <option value="+370"> +370 (Lithuania)</option> <option value="+371"> +371 (Latvia)</option> <option value="+372"> +372 (Estonia)</option> <option value="+380"> +380 (Ukraine)</option> <option value="+381"> +381 (Serbia)</option> <option value="+385"> +385 (Croatia)</option> <option value="+386"> +386 (Slovenia)</option> <option value="+420"> +420 (Czech Republic)</option> <option value="+421"> +421 (Slovakia)</option> <option value="+880"> +880 (Bangladesh)</option> <option value="+971"> +971 (UAE)</option> <option value="+972"> +972 (Israel)</option> <option value="+973"> +973 (Bahrain)</option> <option value="+974"> +974 (Qatar)</option> <option value="+975"> +975 (Bhutan)</option> <option value="+976"> +976 (Mongolia)</option> <option value="+977"> +977 (Nepal)</option> <option value="+992"> +992 (Tajikistan)</option> <option value="+993"> +993 (Turkmenistan)</option> <option value="+994"> +994 (Azerbaijan)</option> <option value="+995"> +995 (Georgia)</option> <option value="+996"> +996 (Kyrgyzstan)</option> <option value="+998"> +998 (Uzbekistan)</option>
+                      {/* ... */}
                     </select>
 
-                    {/* Phone Number */}
                     <input
-                      type="phone"
+                      type="tel"
                       value={phoneNumber}
                       onChange={(e) => setPhoneNumber(e.target.value)}
-
-                      className="flex-1 px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                      className="min-w-[180px] flex-1 px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
                       placeholder="Enter phone number"
                     />
-
                   </div>
+
                   {errors.phone && (
                     <p className="text-red-500 text-sm mt-1">{errors.phone}</p>
                   )}
